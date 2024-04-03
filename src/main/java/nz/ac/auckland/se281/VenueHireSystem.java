7ecd87f3cfccd656428433e88198b7c19e67cc6c
Seerat Mann <sman833@aucklanduni.ac.nz>
2024-04-03 18:17:06 +1300

    Implemented the printBookings method to print an error message when there is no venue found that correlates to the inputted venueCode.

--- src/main/java/nz/ac/auckland/se281/VenueHireSystem.java
+++ src/main/java/nz/ac/auckland/se281/VenueHireSystem.java
@@ -183,7 +183,9 @@ public class VenueHireSystem {
   }
 
   public void printBookings(String venueCode) {
-    // TODO implement this method
+    if (!venueCodes.contains(venueCode)) {
+      MessageCli.PRINT_BOOKINGS_VENUE_NOT_FOUND.printMessage(venueCode);
+    }
   }
 
   public void addCateringService(String bookingReference, CateringType cateringType) {
