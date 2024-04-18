cef9efe0a2a23a6457bb15f4ca080b3ad4ea5006
Seerat Mann <sman833@aucklanduni.ac.nz>
2024-04-18 13:52:56 +1200

    Moved the BookingServices, BookingOperations, and VenueBooking subclasses into their own respective files, that extend the Bookings java superclass

---
+++ src/main/java/nz/ac/auckland/se281/BookingServices.java
@@ -0,0 +1,45 @@
+package nz.ac.auckland.se281;
+
+public class BookingServices extends Bookings {
+  BookingOperations bookingOperations;
+
+  public BookingServices(BookingOperations bookingOperations) {
+    this.bookingOperations = bookingOperations;
+  }
+
+  public void addServiceCatering (String bookingReference, Types.CateringType cateringType) {
+    VenueBooking booking = bookingOperations.getBooking(bookingReference);
+
+    if (booking != null) {
+      String cateringService = cateringType.getName();
+      int cateringPrice = cateringType.getCostPerPerson() * Integer.parseInt(booking.attendees.get(booking.bookingReferences.indexOf(bookingReference)));
+
+      booking.setCateringType(cateringService);
+      booking.setCateringPrice(String.valueOf(cateringPrice));
+    }
+  }
+
+  public void addServiceMusic (String bookingReference) {
+    VenueBooking booking = bookingOperations.getBooking(bookingReference);
+
+    if (booking != null) {
+      String musicService = "Music";
+      int musicPrice = 500;
+
+      booking.setMusicService(musicService);
+      booking.setMusicPrice(String.valueOf(musicPrice));
+    }
+  }
+
+  public void addServiceFloral (String bookingReference, Types.FloralType floralType) {
+    VenueBooking booking = bookingOperations.getBooking(bookingReference);
+
+    if(booking != null) {
+      String floralService = floralType.getName();
+      int floralPrice = floralType.getCost();
+
+      booking.setFloralType(floralService);
+      booking.setFloralPrice(String.valueOf(floralPrice));
+    }
+  }
+}
